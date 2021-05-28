27f40ab1996c51e2ed9c0390befc79ee72a45b7e
Luqmaan Essop <luqmaan.essop@amazee.com>
2021-05-28 15:45:09 +0200

    TODO app precommit + styling

--- Todo/src/showActiveListItems.js
+++ Todo/src/showActiveListItems.js
@@ -13,7 +13,7 @@ function ShowActiveListItems() {
     let status = activeList.items[i].status == "on" ? "" : "checked";
     let duedate = activeList.items[i].dueDate == "" ? "When you get a chance" : activeList.items[i].dueDate;
 
-    listOutput.innerHTML += `<div class="item-todo" data-prio="${priority}" ><label for="item-${i}"><input ${status} type="checkbox" id="item-${i}"></input>${title}</label><span><strong>Due: </strong>${duedate}</span><button class="btn-radius" id="edit-item" type="text">Edit</button></div>`
+    listOutput.innerHTML += `<div class="item-todo" data-prio="${priority}" ><label for="item-${i}"><input ${status} type="checkbox" id="item-${i}"></input>${title}</label><span class="due-date"><strong>Due: </strong>${duedate}</span><button class="btn-radius" id="edit-item" type="text">Edit</button></div>`
   }
 }
 
