62bba08efd355b5115a1d097db5065f557fab957
Luqmaan Essop <luqmaan.essop@amazee.com>
2021-06-17 20:44:28 +0200

    Restaurant page add mobile menu toggler

--- Restaurant-page/src/index.js
+++ Restaurant-page/src/index.js
@@ -4,13 +4,19 @@ import { menuPageContent } from './menupage';
 
 
 window.addEventListener('load', function () {
+
   homePageSwitch();
 
   document.getElementById("contact").addEventListener("click", contactPageSwitch);
   document.getElementById("menu").addEventListener("click", menuPageSwitch);
   document.getElementById("home").addEventListener("click", homePageSwitch);
-
+  document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
 })
+
+function toggleMenu() {
+  let mainNav =  document.getElementById("main-nav");
+  mainNav.classList.contains("visible") === false ? mainNav.classList.add("visible") : mainNav.classList.remove("visible");
+}
 
 function homePageSwitch() {
   const element = document.createElement('div');
