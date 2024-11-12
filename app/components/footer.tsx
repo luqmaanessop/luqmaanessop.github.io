dd39c54e2319dff83fc23b1378509a9d312f1195
Luqmaan Essop <luqmaan.essop@amazee.com>
2024-11-12 08:55:40 +0200

    chore: cleanup

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,6 @@ function SocialLink({ href, icon: Icon }) {
 function SocialLinks() {
   return (
     <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
-      <SocialLink href={socialLinks.linkedin} icon={FaXTwitter} />
       <SocialLink href={socialLinks.github} icon={FaGithub} />
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
