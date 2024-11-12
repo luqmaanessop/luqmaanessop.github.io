db902e004630085151988621dc08d21f3f5869fb
Luqmaan Essop <luqmaan.essop@amazee.com>
2024-11-12 08:43:26 +0200

    chore: update config

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,13 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Luqmaan Essop",
+  name: "Luqmaan",
   ogImage: "/opengraph-image.png",
-  description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+  description: "Personal website of Luqmaan Essop",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/luqmaanessop",
+  linkedin: "https://www.linkedin.com/in/luqmaanessop94/",
+  email: "mailto:essopluqmaan@gmail.com",
 };
