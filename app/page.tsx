3f8337580c33d68bdd262b0f1fa4e27ca53c4b07
Luqmaan Essop <luqmaan.essop@amazee.com>
2024-11-15 07:44:41 +0200

    feat: update profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Page() {
     <section>
       <a href={socialLinks.linkedin} target="_blank">
         <Image
-          src="/profile.png"
+          src="/profile.jpg"
           alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale grayscale-0"
           unoptimized
           width={160}
           height={160}
