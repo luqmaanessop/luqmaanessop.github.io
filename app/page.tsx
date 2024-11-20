071f79c7b57fedb82fb02ad62cad280cb294b620
Luqmaan Essop <luqmaan.essop@amazee.com>
2024-11-20 09:26:03 +0200

    style: theme switcher tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-7xl lg:text-9xl whitespace-nowrap bg-clip-text text-black dark:text-white">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl duration-1000 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-7xl lg:text-9xl whitespace-nowrap bg-clip-text">
         {metaData.title}
       </h1>
 
